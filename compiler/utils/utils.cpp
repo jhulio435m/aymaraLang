a1aef0680681c19406a33ababe11a9eae280e350
Jhulio Alessandro Morán De La Cruz <jhulio435@gmail.com>
2025-06-18 13:59:19 -0500

    Check file open in readFile

--- compiler/utils/utils.cpp
+++ compiler/utils/utils.cpp
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include "error.h"
 #include <fstream>
 #include <sstream>
 
@@ -6,6 +7,10 @@ namespace aym {
 
 std::string readFile(const std::string &path) {
     std::ifstream file(path);
+    if (!file.is_open()) {
+        aym::error("No se pudo abrir el archivo: " + path);
+        return "";
+    }
     std::stringstream buffer;
     buffer << file.rdbuf();
     return buffer.str();
